a52b51808aa5ce2abac4f683141ce9b58caba6cc
shuangrenxu <1919802366@qq.com>
2024-10-11 22:21:15 +0800

    完成解析Token

--- LoxLanguage/Scanner.cs
+++ LoxLanguage/Scanner.cs
@@ -8,6 +8,8 @@ namespace LoxLanguage
 {
     internal class Scanner
     {
+        Dictionary<string, TokenType> keywords;
+
         string source;
         List<Token> tokens = new();
 
@@ -19,12 +21,32 @@ namespace LoxLanguage
         public Scanner(string source)
         {
             this.source = source;
+            keywords = new()
+            {
+                { "and", TokenType.And },
+                { "class", TokenType.Class },
+                { "else", TokenType.Else },
+                { "false", TokenType.False },
+                { "true", TokenType.True },
+                { "for", TokenType.For },
+                { "if", TokenType.If },
+                { "or", TokenType.Or },
+                { "nil", TokenType.Nil },
+                { "return", TokenType.Return },
+                { "this", TokenType.This },
+                { "var", TokenType.Var },
+                { "while", TokenType.While },
+                { "print", TokenType.Print },
+                { "fun",TokenType.Fun },
+                { "base",TokenType.Super}
+            };
         }
 
         public List<Token> scanTokens()
         {
             while (!isEnd)
             {
+                //每次扫描一个Token以后会调整Start下标
                 start = current;
                 ScanToken();
             }
@@ -81,14 +103,154 @@ namespace LoxLanguage
                 case '>':
                     AddToken(Match('=') ? TokenType.Greater_Equal : TokenType.Greater);
                     break;
+
+                case '/':
+                    if (Match('/'))
+                    {
+                        //如果是 //的情况，那么就一直推进到本行的结尾，即一直读取到 结束或者换行符
+                        while (Peek()!='\n' && !isEnd)
+                        {
+                            Advance();
+                        }
+                    }
+                    else
+                    {
+                        AddToken(TokenType.Slash);
+                    }
+                    break;
+
+                case ' ':
+                case '\r':
+                case '\t':
+                    break;
+
+                case '\n':
+                    line++;
+                    break;
+                case '"':
+                    String();
+                    break;
                 default:
-                    Program.Error(line,"Unexpected character");
+                    if (IsDigit(c))
+                    {
+                        Number();
+                    }else if (IsAlpha(c))
+                    {
+                        Identfier();
+                    }
+                    else
+                    {
+                        Program.Error(line,"Unexpected character");
+                    }
                     break;
             }
         }
-        
+
         /// <summary>
-        /// 返回下一个字符
+        /// 转化为标识符,从首个可识别的标识符开始，
+        /// 直到遇到非字母或数字
+        /// </summary>
+        void Identfier()
+        {
+            while (IsAlphaNumeric(Peek()))
+                Advance();
+            string text = source.Substring(start,current);
+     
+            if(!keywords.TryGetValue(text,out var type))
+            {
+                type = TokenType.Identifier;
+            }
+            AddToken(type);
+        } 
+        /// <summary>
+        /// 转化为Number
+        /// </summary>
+        void Number()
+        {
+            //这里之所以先用Peek是因为，当我们读取到了非数字的时候不应该读取字符
+            //而如果直接使用Advance会导致最后一个数字的下一个字符被读取
+            while (IsDigit(Peek()))
+            {
+                Advance();
+            }
+            //小数部分
+            if(Peek()=='.' && IsDigit(PeekNext()))
+            {
+                //越过小数点，
+                Advance();
+                //继续推进
+                while (IsDigit(Peek()))
+                {
+                    Advance();
+                }
+            }
+            AddToken(TokenType.Number,source.Substring(start,current-start));
+        }
+       
+        /// <summary>
+        /// 获得下个字符，依然是不推进
+        /// </summary>
+        /// <returns></returns>
+        char PeekNext()
+        {
+            if (isEnd)
+            {
+                return '\0';
+            }
+            else return source[current+1];
+        }
+        /// <summary>
+        /// 处理数字
+        /// </summary>
+        /// <returns></returns>
+        bool IsDigit(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+
+        /// <summary>
+        /// 判断是否是合法的字母
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        bool IsAlpha(char c)
+        {
+            return c >= 'a' && c <= 'z'
+                || c >= 'A' && c <= 'Z'
+                || c == '_';
+        }
+
+        bool IsAlphaNumeric(char c)
+        {
+            return IsAlpha(c) || IsDigit(c);
+        }
+
+        /// <summary>
+        /// 处理字符串
+        /// </summary>
+        void String()
+        {
+            //当我们读取到了一 个 “，就会尝试读取下一个，但是不推进
+            //一个一个推进直到文件末尾
+            while (Peek() != '"' && !isEnd)
+            {
+                if (Peek() == '\n')
+                    line++;
+                Advance();
+            }
+            if (isEnd)
+            {
+                Program.Error(line,"Unterminated string");
+                return;
+            }
+            ///越过 ",比如 "abc"中c中后面的引号
+            Advance();
+
+            string value = source.Substring(start + 1, current - start -2);
+            AddToken(TokenType.Strings,value);
+        }
+        /// <summary>
+        /// 返回下一个字符,并向前推进一个字符
         /// </summary>
         /// <returns></returns>
         char Advance()
@@ -99,6 +261,7 @@ namespace LoxLanguage
 
         /// <summary>
         /// 判断下一个字符是不是我们想要的字符
+        /// 如果是则会推进一个
         /// </summary>
         /// <param name="c"></param>
         /// <returns></returns>
@@ -113,13 +276,21 @@ namespace LoxLanguage
             current++;
             return true;
         }
+
+        char Peek() {
+            if (isEnd)
+            {
+                return '\0';
+            }
+            else return source[current];
+        }
         void AddToken(TokenType type)
         {
             AddToken(type, null);   
         }
         void AddToken(TokenType type,Object? literal)
         {
-            string text = source.Substring(start, current);
+            string text = source.Substring(start, current - start);
             tokens.Add(new Token(type,text,literal,line));
         }
     }
--- LoxLanguage/obj/Debug/net8.0/LoxLanguage.AssemblyInfo.cs
+++ LoxLanguage/obj/Debug/net8.0/LoxLanguage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoxLanguage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc36c06e1556f31f3b0d5dcaf6e6361a80781653")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoxLanguage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoxLanguage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
